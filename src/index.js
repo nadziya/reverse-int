dfadd494118f3a208713d2d04dbe6dfeab231c69
Nadziya Frolova <sunny1976@mail.ru>
2024-08-06 23:41:19 +0400

    feat: solve the task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString();
+    let len = n.length;
+    let i = 0;
+    let result = '';
+    while (i < len) {
+       let newN = n[i];
+       result = `${newN}${result}`;
+       i = i + 1;
+    }
+    result =  parseInt(result);
+  return result; 
 }
